d6fef938e605ffe0c3a7069dbd0a0e958ce42566
vijethph <vijeth.ph@outlook.com>
2021-01-03 16:06:54 +0530

    add firebase.json

--- src/components/News.vue
+++ src/components/News.vue
@@ -90,7 +90,7 @@ export default {
   data() {
     return {
       user: null,
-      api_key: process.env.NEWSAPI_KEY,
+      api_key: process.env.NEWSAPI_API_KEY,
       articles: [],
       errors: [],
     };
--- src/main.js
+++ src/main.js
@@ -13,13 +13,13 @@ Vue.use(VueSocialSharing);
 Vue.config.productionTip = false;
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
